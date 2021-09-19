bc1d33ae9750f5f37f06d3d346c8ac289afc67b6
Khan <hasan61082@gmail.com>
2021-09-19 14:18:19 +0200

    bean env

--- backend-services.tf
+++ backend-services.tf
@@ -1,3 +1,4 @@
+
 resource "aws_db_subnet_group" "vprofile-rds-subgrp" {
   name       = "main"
   subnet_ids = [module.vpc.private_subnets[0], module.vpc.private_subnets[1], module.vpc.private_subnets[2]]
@@ -9,6 +10,7 @@ resource "aws_db_subnet_group" "vprofile-rds-subgrp" {
 resource "aws_elasticache_subnet_group" "vprofile-ecache-subgrp" {
   name       = "vprofile-ecache-subgrp"
   subnet_ids = [module.vpc.private_subnets[0], module.vpc.private_subnets[1], module.vpc.private_subnets[2]]
+
 }
 
 resource "aws_db_instance" "vprofile-rds" {
@@ -33,7 +35,7 @@ resource "aws_elasticache_cluster" "vprofile-cache" {
   engine               = "memcached"
   node_type            = "cache.t2.micro"
   num_cache_nodes      = 1
-  parameter_group_name = "default.memcached1.6"
+  parameter_group_name = "default.memcached1.5"
   port                 = 11211
   security_group_ids   = [aws_security_group.vprofile-backend-sg.id]
   subnet_group_name    = aws_elasticache_subnet_group.vprofile-ecache-subgrp.name
